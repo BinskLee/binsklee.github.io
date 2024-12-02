50707f9d241ecc48d97ac382d9aec92b4ed9e2dd
binsk-dev <juckmyeol0716@gmail.com>
2024-12-02 22:56:27 +0900

    new post

---
+++ blog/2024-12-01/Improve-async-code-with-event-loop.md
@@ -0,0 +1,12 @@
+---
+slug: improve-async-code-with-event-loop
+title: Improve async code with Promise.all()
+authors: [binsk]
+tags: [javascript, asynchronous, promise, profiling]
+---
+
+Javascript have very powerful asynchronous feature `async/await` that help us to write asynchronous code in a synchronous way so that we can easily understand the code.
+
+However, If you don't pay attention to your code, it can perform inefficiently.
+
+<!-- truncate -->
---
+++ docusaurus.config.ts
@@ -0,0 +1,121 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Binsk's karma",
+  tagline: "For good software",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://binsklee.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "binsklee", // Usually your GitHub org/user name.
+  projectName: "binsklee.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: false,
+        blog: {
+          path: "blog",
+          blogTitle: "Binsk's Karma",
+          blogDescription: "Tech blog by Binsk",
+          routeBasePath: "/",
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            // "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/BinskLee",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        gtag: {
+          trackingID: "G-59T6VNWJWS",
+          anonymizeIP: true,
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Binsk's Karma 🙏",
+      items: [
+        { to: "/", label: "Posts", position: "left" },
+        {
+          href: "https://github.com/BinskLee",
+          label: "GitHub Profile",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Community",
+          items: [
+            {
+              label: "LinkedIn",
+              href: "https://linkedin.com/in/yongjoon-lee-binsk",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Binsk's karma.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
